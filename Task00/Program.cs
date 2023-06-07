3cf27e7c1d28b73b6306a11ede229a46b898fee5
Your Name <you@example.com>
2023-06-07 16:01:42 +0300

    Нахождение квадрата введенного числа пользователем

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая на вход принимает число и выводит его квадрат
+// (число умноженное на само себя)
+
+// Например: 
+//  4 -> 16
+//  -3 -> 9
+//  -7 -> 49
+
+Console.Write("Введите число, квадрат которого нужно найти: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int square = number * number;
+
+Console.WriteLine($"Квадрат числа {number} = {square}");
